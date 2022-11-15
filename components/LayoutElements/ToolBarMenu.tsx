11bcdd2d1e0ea3765afd730bbdb78dcae9a8e05a
Theo Oz <theoozdev@gmail.com>
2022-11-15 09:15:47 +0100

    missing file

--- components/LayoutElements/ToolBarMenu.tsx
+++ components/LayoutElements/ToolBarMenu.tsx
@@ -6,7 +6,7 @@ const ToolBarMenu = () => {
   const [expandToolBar, setExpandToolBar] = useState<boolean>(false);
   return (
     <div
-      className={`fixed md:sticky h-screen ${
+      className={`fixed md:sticky h-screen z-50 ${
         expandToolBar
           ? "left-0 top-0 w-4/5 md:w-2/5 mx-2 bg-gray-50 dark:bg-gray-900"
           : "top-2 left-2 text-3xl rounded-full m-2"
@@ -14,7 +14,7 @@ const ToolBarMenu = () => {
     >
       <button
         onClick={() => setExpandToolBar(!expandToolBar)}
-        className="sticky md:absolute top-2 left-2 text-3xl border p-2 rounded-full m-2"
+        className="sticky md:absolute top-2 left-0 text-3xl border p-2 rounded-full"
       >
         {expandToolBar ? <HiX /> : <HiOutlineMenu />}
       </button>
