07cc64b4122b14e943a0d6c82b0714b43011aa8c
Theo Oz <theoozdev@gmail.com>
2022-11-15 09:31:55 +0100

    meta data

--- components/LayoutElements/HeadElement.tsx
+++ components/LayoutElements/HeadElement.tsx
@@ -8,20 +8,35 @@ const HeadElement = () => {
       <meta name="description" content="Displays React issues." />
       <link rel="icon" href="/favicon.ico" /> {/* Open Graph / Facebook */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="/" />
-      <meta property="og:title" content="Theo's DWD Client" />
+      <meta
+        property="og:url"
+        content="https://theo-s-issue-displayer.vercel.app/"
+      />
+      <meta
+        property="og:title"
+        content="Theo's GitHub issue displayer for React.js"
+      />
       <meta
         property="og:description"
-        content="A beautiful web app to query and display historical weather data of DWD OpenData."
+        content="Theo's GitHub issue displayer for React.js"
       />
-      <meta property="og:image" content="/meta.png" />
+      <meta
+        property="og:image"
+        content="https://theo-s-issue-displayer.vercel.app/meta.png"
+      />
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="/" />
-      <meta property="twitter:title" content="Theo's DWD Client" />
+      <meta
+        property="twitter:url"
+        content="https://theo-s-issue-displayer.vercel.app/"
+      />
+      <meta
+        property="twitter:title"
+        content="Theo's GitHub issue displayer for React.js"
+      />
       <meta
         property="twitter:description"
-        content="A beautiful web app to query and display historical weather data of DWD OpenData."
+        content="Theo's GitHub issue displayer for React.js"
       />
       <meta property="twitter:image" content="/meta.png" />
     </Head>
